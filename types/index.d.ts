b2dbdfb3f5185c46640722315ded4f7cc96a21de
Leza Morais Lutonda <lemol-c@hotmail.com>
2019-10-03 14:51:15 +0100

    types: declare usePrevProp with strong types (#4)

--- types/index.d.ts
+++ types/index.d.ts
@@ -2,7 +2,7 @@ declare module '@use-it/prev-prop' {
   /**
    * A custom React Hook that maintains a previous value for a prop (or any other value).
    */
-  export default function usePrevProp(
-    prop: any
-  ): any;
+  export default function usePrevProp<TProp = any>(
+    prop: TProp
+  ): TProp | undefined;
 }
